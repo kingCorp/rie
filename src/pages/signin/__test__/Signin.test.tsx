57cc900640074721b0fcf1b69608d40872cca3e7
Attasiem Kingsley <attasiemj@gmail.com>
2022-11-13 17:45:08 +0100

    fix: change password organizer

--- src/pages/signin/__test__/Signin.test.tsx
+++ src/pages/signin/__test__/Signin.test.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
-import Signin from './pages/signin/Signin';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { store } from '../../../redux/store';
+import Signin from '../Signin';
 
 const MockRouterSignin = () => (
   <BrowserRouter>
